2e20260982c0de70a3964fa4f192caf3b845f2d5
Dschnepf7 <domschnepf1@gmail.com>
2023-03-27 21:02:58 -0500

    added more code to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+  const jateDb = await openDB('jate', 1);
+  
+    // Create a new transaction and specify the database and data privileges.
+    const tx = jateDb.transaction('jate', 'readwrite');
+  
+    // Open up the desired object store.
+    const store = tx.objectStore('jate');
+  
+    // Use the .add() method on the store and pass in the content.
+    const request = store.put({id:1,value:content});
+  
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+    return result;
+  };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => 
